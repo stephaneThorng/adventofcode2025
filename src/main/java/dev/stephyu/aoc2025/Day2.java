01d4a00a534f5787da5658b499ae8b4dba87099f
Stéphane THORNG <stephane.thorng@gmail.com>
2025-12-11 14:05:15 +0100

    feat: day2 solution

---
+++ src/main/java/dev/stephyu/aoc2025/Day2.java
@@ -0,0 +1,48 @@
+package dev.stephyu.aoc2025;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Day2 {
+    public long play(String input) {
+        long result = 0;
+        String[] ranges = input.split(",");
+        for (String range : ranges) {
+            String[] bounds = range.split("-");
+            long left = Long.parseLong(bounds[0].trim());
+            long right = Long.parseLong(bounds[1].trim());
+            for (long i = left; i <= right; i++) {
+                if (isInvalidID(i)) {
+                    result += i;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int numberOfDigits(long number) {
+        return (int) Math.log10(number) + 1;
+    }
+
+    private boolean isInvalidID(long number) {
+        int nbDigit = numberOfDigits(number);
+        String s = String.valueOf(number);
+
+        for (int nbPart = 2; nbPart <= nbDigit; nbPart++) {
+            if (nbDigit % nbPart == 0) {
+                int partSize = nbDigit / nbPart;
+                Set<String> parts = new HashSet<>();
+
+                for (int i = 0; i < nbPart; i++) {
+                    parts.add(s.substring(i * partSize, (i + 1) * partSize));
+                }
+
+                if (parts.size() == 1) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/dev/stephyu/aoc2025/Day2Test.java
@@ -0,0 +1,38 @@
+package dev.stephyu.aoc2025;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Day2Test {
+
+    private static Stream<Arguments> arguments() {
+        return Stream.of(
+                Arguments.of("day2_sample.txt", 4174379265L),
+                Arguments.of("day2_puzzle.txt", 24774350322L)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("arguments")
+    void should_return_against_input_file(String filePath, long expected) throws IOException, URISyntaxException {
+        Path path = Paths.get(Objects.requireNonNull(getClass().getClassLoader()
+                .getResource(filePath)).toURI());
+        Stream<String> lines = Files.lines(path);
+        String content = lines.collect(Collectors.joining("\r\n"));
+        lines.close();
+
+        assertEquals(expected, new Day2().play(content));
+    }
+}
